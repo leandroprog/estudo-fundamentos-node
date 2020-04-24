84ede6df991e20df9ed7bd6f42375da6779c5910
Leandro Rocha <prog.leandro@gmail.com>
2020-04-23 22:36:58 -0300

    Finalizando o desafio

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,35 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const balance = this.transactions.reduce(
+      (acc: Balance, item: Transaction) => {
+        const incomeValue = item.type === 'income' ? item.value : 0;
+        const outcomeValue = item.type === 'outcome' ? item.value : 0;
+
+        acc.income += incomeValue;
+        acc.outcome += outcomeValue;
+        acc.total += incomeValue - outcomeValue;
+        return acc;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
